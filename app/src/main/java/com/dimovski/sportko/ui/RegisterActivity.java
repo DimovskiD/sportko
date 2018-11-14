d88353e4738ef8cbb4877b0d0c2df57f95ea2329
Daniel Dimovski <dimovski.d@outlook.com>
2018-11-14 13:35:34 +0100

    Initial commit

---
+++ app/src/main/java/com/dimovski/sportko/ui/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.dimovski.sportko.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.dimovski.sportko.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/main/java/com/dimovski/sportko/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package com.dimovski.sportko.utils;
+
+public class StringUtils {
+
+    public static boolean isEmpty(String x) {
+        return x.trim().length() == 0;
+    }
+}
